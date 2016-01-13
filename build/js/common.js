f054322e257d4228e6925664ad9080e12d8b46f7
Ex-Zy <serega1905@ukr.net>
2016-01-13 18:38:35 +0200

    upd last

--- build/js/common.js
+++ build/js/common.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
 
-	$('.menu-panel__holiday').click(function() {
-		if ($(this).hasClass('is-active')) {
-			$('.menu-panel__holiday').removeClass('is-active');
-		}
-		else {
-			$('.menu-panel__holiday').removeClass('is-active');
-			$(this).addClass('is-active');
-		}
-	});
+	// $('.menu-panel__holiday').click(function() {
+	// 	if ($(this).hasClass('is-active')) {
+	// 		$('.menu-panel__holiday').removeClass('is-active');
+	// 	}
+	// 	else {
+	// 		$('.menu-panel__holiday').removeClass('is-active');
+	// 		$(this).addClass('is-active');
+	// 	}
+	// });
+
+	// $("body").wrapInner("<div class='wrp'><div class='wrp-inner'></div></div>");
 
 });
